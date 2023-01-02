d0e35aaeeaf7cdd739792a9ad78ef914c4311504
Priyanshu Pathak <39450902+Priyanshu-1012@users.noreply.github.com>
2023-01-03 04:16:07 +0530

    day1
    full adder

---
+++ Day1-FullAdder/FullAdder_tb.v
@@ -0,0 +1,33 @@
+module tb();
+  reg a,b,cin;
+  wire sum,cout;
+  full_add FA (sum,cout,a,b,cin);
+  
+  initial begin 
+    a=0;b=0;cin=0;
+  end
+  
+  initial begin
+    #5 a=1;b=0;cin=0;
+    #5 a=1;b=1;cin=0;
+    #5 a=0;b=0;cin=1;
+    #5 a=0;b=1;cin=0;
+    #5 a=1;b=1;cin=1;
+   
+  end
+    
+ //Dump waves (only required here)
+initial
+  begin
+    $dumpfile("dump.vcd");
+    $dumpvars(1);
+  end  
+  
+ initial begin
+   $monitor($time,"a=%b,b=%b,cin=%b,sum=%b,cout=%b",a,b,cin,sum,cout);
+ #30 $finish;
+   
+ end
+endmodule
+
+ 
